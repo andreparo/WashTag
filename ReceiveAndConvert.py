b2c752d46b10089e1e2432ebde5484e7661975af
Andrea.Parodi <aparodi@unibz.it>
2022-11-12 14:08:50 +0100

    recive and convert

--- ReceiveAndConvert.py
+++ ReceiveAndConvert.py
@@ -1,3 +1,23 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+
+t_end = time.time() + 5000
+while time.time() < t_end:
+
+	url = "http://192.168.43.254"
+
+	page = requests.get(url)
+
+	soup = BeautifulSoup(page.content,'html.parser')
+
+	rows = soup.find_all('tr')
+
+	output = str(rows[0])[8:-10]
+
+	print(output)
+#------------------------------------------------------------------------------------
+
 
 def DevideString(inputStr):
     lastHashstag = -1
@@ -12,13 +32,21 @@ def DevideString(inputStr):
 
     return res
 
-
-
 def CompressInfo(temperature, material, color, cycles, extraInfo):
     res = 90 * temperature + material * 30 + color * 10 + cycles
     #res = str(res) + extraInfo
     #print("4: " + str(res))
-    return res
+    
+    return str(res) + extraInfo
+
+
+def multiCompress(array):
+    res = ""
+    for a in array:
+        res = res + str(CompressInfo(a[0],a[1],a[2],a[3], a[4]))
+        res = res + "#"
+    print(res)
+
 
 def DecompressInfo(info):
     #info is array in this format ["123ADIDAS", "654NIKE"]
@@ -46,5 +74,6 @@ def DecompressInfo(info):
 testString = "123ADIDAS#456NIKE#"
 print(DevideString(testString))
 
-#box = CompressInfo(5,1,2,9,"hi")
+testCompress = [[1,1,2,7,"Red Wool Jumper"],[3,0,0,2,"White Cotton T-shirt"]]
+multiCompress(testCompress)
 print(DecompressInfo(DevideString(testString)))
